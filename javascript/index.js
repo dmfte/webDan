09d6a161f70f4d2601c6df964bc2d19a9dc7e5e6
dmfte <62442934+dmfte@users.noreply.github.com>
2020-05-22 18:34:50 -0600

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+// Code to set fixed-navigation bar height, so it doesn´t cover body content
+var ht = "" +document.querySelector("#navbar1").offsetHeight+ "px";
+document.querySelector(".divHeight").style.height = ht;
